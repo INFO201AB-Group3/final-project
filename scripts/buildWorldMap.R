587b3c18df5acdfe2bc3043de7f9b8eca358eaf1
Mithula Rarmal <rmithula@uw.edu>
2016-12-06 16:07:17 -0800

    blah

--- scripts/buildWorldMap.R
+++ scripts/buildWorldMap.R
@@ -3,21 +3,23 @@ library(plotly)
 
 df <- read.csv('OECD_stats.csv')
 
-data <- df %>%
-  select(Country., Sex, Field, Year., Value, Level.of.education) %>%
-  filter(Year. == 2014) %>%
-  filter(Field == "Science, mathematics and computing") %>%
-  filter(Sex == "Women") %>%
-  filter(Level.of.education == "Total tertiary education (ISCED2011 levels 5 to 8)") %>%
-  filter(Country. != "Belgium") %>%
-  filter(Country. != "China (People's Republic of)") %>%
-  filter(Country. != "Israel") %>%
-  filter(Country. != "South Africa")
-
+dataset <- na.omit(df)
+  data <- dataset %>%
+    select(Country., Sex, Field, Year., Value, Level.of.education) %>%
+    filter(Year. == 2014) %>%
+    filter(Field == "Science, mathematics and computing") %>%
+    filter(Sex == "Women") %>%
+    filter(Level.of.education == "Total tertiary education (ISCED2011 levels 5 to 8)") %>%
+    
+    
+    data$Code <- c("AUS" ,"AUT", "CZE","DNK", "FIN", "FRA", "DEU", "GRC", "HUN","IRL",
+                   "ITA", "JPN"," PRK", "LUX", "MEX","NLD", "NZL", "NOR", "POL","PRT",
+                   "SVK", "ESP", "SWE","CHE", "TUR","GBR", "USA", "BRA", "CHL","COL",
+                   "EST","IDN","LVA", "RUS", "SAU", "SVN")
 
 
 # light grey boundaries
-l <- list(color = toRGB("grey"), width = 0.5)
+l <- list(color = toRGB("grey"), width = 1)
 
 # specify map projection/options
 g <- list(
@@ -27,12 +29,12 @@ g <- list(
 )
 
 p <- plot_geo(data) %>%
-  add_trace(
+  add_trace( 
     z = ~Value, color = ~Value, colors = 'Blues',
-    text = ~Country., locations = ~Country., marker = list(line = l)
+    text = ~Country., locations = ~Code, marker = list(line = l)
   ) %>%
-  #colorbar(title = 'GDP Billions US$', tickprefix = '$') %>%
+  colorbar(title = 'Percentage') %>%
   layout(
-    title = '2014 >',
+    title = 'Comparing Total Tertiary Education Around the World in 2014',
     geo = g
   )
